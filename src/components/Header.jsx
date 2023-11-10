881c881f7c84103877bfad3de1c3e03bb418794f
hassaammgl <h455a4mmehtab@gmail.com>
2023-11-11 03:09:59 +0500

    upload to git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import logo from "../assets/logo.png";
 import "./header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to={"/"}>
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
